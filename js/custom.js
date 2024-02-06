3df1e01e3e652f75e536bd9f6b886f31e8e29aca
kahmed0984 <kahmed0984@gmail.com>
2024-02-06 20:21:04 +0600

    add

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,20 @@
 // 	});
 // });
 
+$(document).ready(function(){
+	window.onscroll = function() {myFunction()};
 
+                var navbar = document.getElementById("amar_");
+                var sticky = navbar.offsetTop;
+
+            function myFunction() {
+                if (window.pageYOffset >= sticky) {
+                    navbar.classList.add("sticky")
+            } else {
+                navbar.classList.remove("sticky");
+                }
+            }
+});
 
 
 
