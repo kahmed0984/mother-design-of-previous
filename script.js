b26ae8b67dd0e0570a38a0426415e4ba16e874d6
kahmed0984 <kahmed0984@gmail.com>
2024-03-30 16:49:41 +0600

    add some javascriot by softech it

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+document.getElementById('service_one').onclick = function(){
+	document.getElementById('service_contant_one').style.display = 'none';
+}
+document.getElementById('service_tow').onclick = function(){
+	document.getElementById('service_contant_one').style.display = 'block';
+}
+
+document.getElementById('service_three').onclick = function(){
+	var showContant = document.getElementById('service_contant_three').style.display;
+
+	if( showContant === 'none'){
+		document.getElementById('service_contant_three').style.display = 'block';
+	}
+	else if( showContant === 'block'){
+		document.getElementById('service_contant_three').style.display = 'none';
+	}
+	else{
+		document.getElementById('service_contant_three').style.display = 'none';
+	}
+}
